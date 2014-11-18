9b6aa421d5db11bcbdbffa07068160629f6481be
awortham <awortham@newlifechurch.tv>
2014-11-18 13:08:19 -0700

    adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   root to: "trails#index"
   resources :comments
-
   resources :usertrails
-
   resources :trails
-
   resources :users
 
   get '/auth/:provider/callback', to: 'sessions#create', as: 'twitter'
