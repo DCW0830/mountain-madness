79c2122abd2c9b12d34fe2b0d9efe98db9d4c11b
awortham <awortham@newlifechurch.tv>
2014-11-18 20:22:14 -0700

    added keys to secret file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "iNgWm4eTzltC5NGOaxXlqD6p4", "OZdfW65SrSB2RDeROsPMROxkEtYofA7NiTWqQIj4kpFeTIqlSr",
-    {
-        :secure_image_url => 'true',
-        :image_size => 'original',
-        :authorize_params => {
-          :force_login => 'true',
-          :lang => 'pt'
-        }
-      }
+  provider :twitter, TWITTER_KEY, TWITTER_SECRET
 end
