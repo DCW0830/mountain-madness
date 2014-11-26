fc144cfafa08352ed285a41f82c850316df7e27f
awortham <awortham@newlifechurch.tv>
2014-11-26 12:19:26 -0700

    api work

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,5 +1,48 @@
 class Api::V1::TrailsController < Api::V1::BaseController
+  force_ssl unless Rails.env.development?
+
+  before_action :authenticate, except: [:index, :show]
+
+  respond_to :json, :xml
   def index
     respond_with Trail.all
+  end
+  def show
+    respond_with Trail.find(params[:id])
+  end
+  def create
+    respond_with Trail.create(trail_params)
+  end
+  def destroy
+    respond_with Trail.destroy(params[:id])
+  end
+  def update
+    respond_with Trail.update(params[:id], article_params)
+  end
+
+  def trail_params
+    params.require(:trail).permit(:unique_id,
+                                  :distance,
+                                  :description,
+                                  :name,
+                                  :city,
+                                  :state,
+                                  :lat,
+                                  :lon,
+                                  :directions,
+                                  :rating,
+                                  :url,
+                                  :thumbnail
+                                  )
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic("Where's Your work visa?") do |email, password|
+      author = Author.find_by(email: email)
+
+      return true if author && author.authenticate(password)
+
+      head :unauthorized
+    end
   end
 end
