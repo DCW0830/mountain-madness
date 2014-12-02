a69d8503b1a7402561fdd363cdfab776c6fe903a
awortham <awortham@newlifechurch.tv>
2014-12-02 13:58:58 -0700

    trying to add delete button to dashboard

--- app/controllers/usertrails_controller.rb
+++ app/controllers/usertrails_controller.rb
@@ -1,2 +1,9 @@
 class UsertrailsController < ApplicationController
+
+  def destroy
+    usertrail = current_user.trails.find(params[:id])
+    usertrail.destroy
+    flash[:notice] = "You have removed the trail from your personal dashboard."
+    redirect_to dashboard_index_path
+  end
 end
