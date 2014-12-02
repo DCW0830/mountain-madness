933937e68ef6be94fdc00d50fc178ca5858c2632
awortham <awortham@newlifechurch.tv>
2014-12-02 10:26:19 -0700

    trail wrapper

---
+++ lib/trail_wrap.rb
@@ -0,0 +1,25 @@
+module TrailWrap
+  class TrailUser
+    attr_reader :id
+
+    def initialize(id = '')
+      @id = id
+    end
+
+    def url
+      'http://192.241.153.158/api/v1/trails'
+    end
+
+    def build_url
+      if @id.empty?
+        "#{url}.json"
+      else
+        "#{url}/#{id}.json"
+      end
+    end
+
+    def make_request
+      (Faraday.get(build_url))
+    end
+  end
+end
